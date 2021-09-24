143ce6a00c5c0f80a6a1b2a0de5547bb50e3d7f1
manish <manish.sharma@antechindia.com>
2021-09-24 12:45:08 +0530

    add nft

--- api_service/src/modules/nft/nft.schema.ts
+++ api_service/src/modules/nft/nft.schema.ts
@@ -1,5 +1,4 @@
-import mongoose, { Schema } from "mongoose";
-import mongooseUniqueValidator from 'mongoose-unique-validator';
+import mongoose, { Schema } from "mongoose"
 
 class NftSchema extends Schema {
    
@@ -14,23 +13,22 @@ class NftSchema extends Schema {
         this.schema = new Schema({
             nftAddress: {type: String, required:true, trim:true},
             name: { type: String, required: true },
-            fileHash: { type: String, required: true },
+            fileType: {type: String, default: 'image'},
+            fileHash: { type: String, required: true },//file uploaded to ipfs
             externalLink: { type: String, default: "" },
             description: { type: String, default: "" },
-            tokenUri: { type: String, default: "" },
+            tokenUri: { type: String, default: "" },//for meta data
             supply: { type: Number, default: 1 },
             royality: { type: Number, max: 100, min: 0, required: true },
             networkId: { type: String, required: true },
             blockchain: { type: String },
-            sensitiveContent: { type: String },
             status: { type: String, default: 'ACTIVE' },
             tokenId: {type: Number, default: 0},
-            owner:{type: mongoose.Schema.Types.ObjectId,ref: "User", required: true },
-            creator: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+            owner:{type: Schema.Types.ObjectId,ref: "User", required: true },
+            creator: { type: Schema.Types.ObjectId, ref: "User", required: true },
+            collection: { type: Schema.Types.ObjectId, ref: "Collection", required: true },
         }, { timestamps: true })
-
-        this.schema.plugin(mongooseUniqueValidator, { type: 'mongoose-unique-validator' });
     }
 }
 
-export default mongoose.model('Collection', new NftSchema().schema);
+export default mongoose.model('nft', new NftSchema().schema);
